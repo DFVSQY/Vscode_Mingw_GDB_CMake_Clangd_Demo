5e9c762d4c7fe355c17d89a336be67129434f4c2
DFVSQY <dfvsqy@qq.com>
2022-10-12 23:20:24 +0800

    增加多个源文件支持

---
+++ src/c_math.c
@@ -0,0 +1,11 @@
+#include "c_math.h"
+
+int add(int a, int b) {
+    int c = a + b;
+    return c;
+}
+
+int sub(int a, int b) {
+    int c = a - b;
+    return c;
+}
--- src/main.c
+++ src/main.c
@@ -1,6 +1,13 @@
+#include "c_math.h"
 #include <stdio.h>
 
 int main() {
-    printf("hello world");
+    printf("hello world\n");
+
+    int a = 20, b = 50;
+    int c = add(a, b);
+    int d = add(a, b);
+    printf("c:%d d:%d\n", c, d);
+
     return 0;
 }
