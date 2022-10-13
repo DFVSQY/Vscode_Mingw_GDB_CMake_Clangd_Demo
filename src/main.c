b83b6f5b35c887b4194d626a036664260ef51fb8
DFVSQY <dfvsqy@qq.com>
2022-10-14 00:44:39 +0800

    增加Clang-Tidy检测

--- src/main.c
+++ src/main.c
@@ -1,6 +1,34 @@
 #include "c_math.h"
 #include <stdio.h>
 
+void test_too_small_loop_variable() {
+    long size = 294967296l;
+    /*
+    This for loop is an infinite loop because the short type can’t represent all
+    values in the [0..size] interval.
+
+    Clang-Tidy Warning:
+    Loop variable has narrower type 'short' than iteration's upper bound 'long'.
+    */
+    for (short index = 0; index < size; ++index) {
+        printf("%d\n", index);
+    }
+}
+
+void test_terminating_continue(int a) {
+    do {
+        /*
+        Clang-Tidy Warning:
+        'continue' in loop with false condition is equivalent to 'break' (fix
+        available)
+        */
+        if (a > 0)
+            continue;
+
+        a++;
+    } while (0);
+}
+
 int main() {
     printf("hello world\n");
 
